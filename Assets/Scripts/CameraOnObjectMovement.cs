5be87632b5d49a22dcec63b35e8e31f0e2a77d5b
Renzak <33142632+Renzak@users.noreply.github.com>
2020-06-01 23:01:07 +0300

    CameraAndMovementSync
    Co-Authored-By: drumi <drumi@users.noreply.github.com>

--- Assets/Scripts/CameraOnObjectMovement.cs
+++ Assets/Scripts/CameraOnObjectMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class CameraOnObjectMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float MOUSE_SENSITIVITY = 0.1f;
+    public float MAX_COS = 0.4f;
+    public float MIN_COS = 0.1f;
+
+    GameObject mainCamera;
+    
+    readonly string CAMERA_NAME = "MainCamera";
+
+    const int CAMERA_RADIUS = 10;
+
+    float oldX;
+    float oldY;
+
     void Start()
     {
-        
+        oldX = 0f;
+        oldY = Mathf.Acos((MIN_COS + MAX_COS) / 2f);
+        mainCamera = GameObject.Find(CAMERA_NAME);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = MOUSE_SENSITIVITY * -Input.GetAxis("Mouse X");
+        float mouseY = MOUSE_SENSITIVITY * -Input.GetAxis("Mouse Y");
+        UpdateCameraPosition(mouseX, mouseY);
+
     }
+
+    void UpdateCameraPosition(float mouseX, float mouseY)
+    {
+        float cosA;
+        float cosB;
+
+        float sinA;
+        float sinB;
+
+        if (Mathf.Cos(mouseY + oldY) > MAX_COS || Mathf.Cos(mouseY + oldY) < MIN_COS)
+        {
+            mouseY = 0;
+        }
+
+        cosA = Mathf.Cos(mouseX + oldX);
+        cosB = Mathf.Cos(mouseY + oldY);
+
+        sinA = Mathf.Sin(mouseX + oldX);
+        sinB = Mathf.Sin(mouseY + oldY);
+
+        float x = CAMERA_RADIUS * cosA * sinB;
+        float y = CAMERA_RADIUS * cosB;
+        float z = CAMERA_RADIUS * sinA * sinB;
+
+        mainCamera.transform.position = gameObject.transform.position + new Vector3(x, y, z);
+        mainCamera.transform.LookAt(gameObject.transform);
+
+        oldX += mouseX;
+        oldY += mouseY;
+    }
+
 }
--- Assets/Scripts/MovementControllerWithCamera.cs
+++ Assets/Scripts/MovementControllerWithCamera.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class MovementControllerWithCamera : MonoBehaviour
 {
-    GameObject mainCamera;
     Rigidbody rigidBody;
+    public GameObject mainCamera;
 
     readonly Vector3 VERTICAL_VELOCITY = new Vector3(0, 30, 0);
     readonly string JUMPABLE_TAG = "JumpPlatform";
-    readonly string CAMERA_NAME = "MainCamera";
 
-    const int SPEED_FACTOR = 50;
-    const int CAMERA_DISTANCE = 10;
-    const int CAMERA_HEIGHT = 7;
-    const int MAX_VELOCITY = 20;
+    public int SPEED_FACTOR = 50;
+    public int MAX_VELOCITY = 20;
 
     bool canJump = false;
 
     void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody>();
-        mainCamera = GameObject.Find(CAMERA_NAME);
     }
 
     void Update()
     {
-        UpdateCameraPosition();
         ProccessUserInput();
     }
 
@@ -56,44 +51,43 @@ public class MovementControllerWithCamera : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            rigidBody.velocity += new Vector3(-1 * SPEED_FACTOR * Time.deltaTime, 0, 0);
-            if (rigidBody.velocity.x < -MAX_VELOCITY)
-            {
-                rigidBody.velocity = new Vector3(-MAX_VELOCITY, rigidBody.velocity.y, rigidBody.velocity.z);
-            }
+            rigidBody.velocity += Quaternion.Euler(0, -90, 0) * new Vector3(transform.position.x - mainCamera.transform.position.x, 0, transform.position.z - mainCamera.transform.position.z).normalized * Time.deltaTime * SPEED_FACTOR;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            rigidBody.velocity += new Vector3(0, 0, 1 * SPEED_FACTOR * Time.deltaTime);
-            if (rigidBody.velocity.z > MAX_VELOCITY)
-            {
-                rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, MAX_VELOCITY);
-            }
+            rigidBody.velocity += new Vector3(transform.position.x - mainCamera.transform.position.x, 0, transform.position.z - mainCamera.transform.position.z).normalized * Time.deltaTime * SPEED_FACTOR;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            rigidBody.velocity += new Vector3(1 * SPEED_FACTOR * Time.deltaTime, 0, 0);
-            if (rigidBody.velocity.x > MAX_VELOCITY)
-            {
-                rigidBody.velocity = new Vector3(MAX_VELOCITY, rigidBody.velocity.y, rigidBody.velocity.z);
-            }
+            rigidBody.velocity += Quaternion.Euler(0, -90, 0) * -new Vector3(transform.position.x - mainCamera.transform.position.x, 0, transform.position.z - mainCamera.transform.position.z).normalized * Time.deltaTime * SPEED_FACTOR;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            rigidBody.velocity += new Vector3(0, 0, -1 * SPEED_FACTOR * Time.deltaTime);
-            if (rigidBody.velocity.z < -MAX_VELOCITY)
-            {
-                rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -MAX_VELOCITY);
-            }
+            rigidBody.velocity -= new Vector3(transform.position.x - mainCamera.transform.position.x, 0, transform.position.z - mainCamera.transform.position.z).normalized * Time.deltaTime * SPEED_FACTOR;
         }
-    }
 
-    void UpdateCameraPosition()
-    {
-        mainCamera.transform.position = gameObject.transform.position + new Vector3(0, CAMERA_HEIGHT, -CAMERA_DISTANCE);
+        if (rigidBody.velocity.x > MAX_VELOCITY)
+        {
+            rigidBody.velocity = new Vector3(MAX_VELOCITY, rigidBody.velocity.y, rigidBody.velocity.z);
+        }
+
+        if (rigidBody.velocity.z < -MAX_VELOCITY)
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -MAX_VELOCITY);
+        }
+
+        if (rigidBody.velocity.z > MAX_VELOCITY)
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, MAX_VELOCITY);
+        }
+
+        if (rigidBody.velocity.x < -MAX_VELOCITY)
+        {
+            rigidBody.velocity = new Vector3(-MAX_VELOCITY, rigidBody.velocity.y, rigidBody.velocity.z);
+        }
     }
 
 }
