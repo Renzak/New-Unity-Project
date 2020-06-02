85110afe92856527b1beef508e06274c0ede861e
drumi <Software1251.gmail.com>
2020-06-02 21:42:26 +0300

    added rotation script

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+    public enum Axis
+    {
+        x,
+        y,
+        z,
+    }
+
+    public Axis axis;
+    public float rotationSpeed;
+
+    void Update()
+    {
+        gameObject.transform.Rotate(
+            new Vector3( axis == Axis.x? 1 : 0, axis == Axis.y? 1 : 0, axis == Axis.z? 1 : 0), rotationSpeed * Time.deltaTime);
+    }
+}
